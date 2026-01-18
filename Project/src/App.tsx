acd5b670a784df8b16347d8154f3fddb65b8a78e
meirChulpaev1 <israel1948and@gmail.com>
2026-01-18 17:13:55 +0200

    fixed

--- Project/src/App.tsx
+++ Project/src/App.tsx
@@ -18,7 +18,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/quote" element={<Quotes />} />
 
-          <Route path="*" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
 
         </Routes>
       </BrowserRouter>
